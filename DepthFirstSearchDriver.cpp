e239b3514f8171429346ef38d18ed62a24553632
Brian Heim <brianlheim@gmail.com>
2017-07-02 14:59:25 -0400

    Add driver for depth first search

---
+++ DepthFirstSearchDriver.cpp
@@ -0,0 +1,41 @@
+/** \file DepthFirstSearch.cpp
+ * Driver for DepthFirstSearch
+ *
+ * \author Brian Heim
+ * \date   2017-06-30
+ */
+
+#include "UndirectedGraph.hpp"
+#include "DepthFirstSearch.hpp"
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+using VertexID = UndirectedGraph::VertexID;
+
+int main()
+{
+  cout << "Driver for depth first search on undirected graph" << endl;
+  cout << "Enter the graph:" << endl;
+
+  UndirectedGraph ug = UndirectedGraph( cin );
+
+  cout << "The graph is this: " << endl;
+  cout << ug.toString();
+
+  VertexID v_id;
+  while (true) {
+    cout << "Enter the vertex to search with, or a non-number to quit: ";
+    cin >> v_id;
+    if ( !cin )
+      break;
+
+    DepthFirstSearch<UndirectedGraph, VertexID> dfs( ug, v_id );
+
+    cout << "There are " << dfs.count() << " vertices connected to " << v_id << endl;
+  }
+
+  return 0;
+}
