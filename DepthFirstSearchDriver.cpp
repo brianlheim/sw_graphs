77ea986e065c97e9fb7374975f7e0854a6fa413e
Brian Heim <brianlheim@gmail.com>
2017-07-03 10:37:33 -0400

    DepthFirstSearch: add polymorphism

--- DepthFirstSearchDriver.cpp
+++ DepthFirstSearchDriver.cpp
@@ -5,22 +5,25 @@
  * \date   2017-06-30
  */
 
+#include "AbstractUGraph.hpp"
+#include "AMUGraph.hpp"
 #include "ALUGraph.hpp"
 #include "DepthFirstSearch.hpp"
+
 #include <iostream>
 
 using std::cin;
 using std::cout;
 using std::endl;
 
-using VertexID = UndirectedGraph::VertexID;
+using VertexID = AbstractUGraph::VertexID;
 
 int main()
 {
   cout << "Driver for depth first search on undirected graph" << endl;
   cout << "Enter the graph:" << endl;
 
-  UndirectedGraph ug = UndirectedGraph( cin );
+  ALUGraph ug = ALUGraph( cin );
 
   cout << "The graph is this: " << endl;
   cout << ug.toString();
@@ -32,7 +35,7 @@ int main()
     if ( !cin )
       break;
 
-    DepthFirstSearch<UndirectedGraph, VertexID> dfs( ug, v_id );
+    DepthFirstSearch<ALUGraph, VertexID> dfs( ug, v_id );
 
     cout << "There are " << dfs.count() << " vertices connected to " << v_id << endl;
   }
