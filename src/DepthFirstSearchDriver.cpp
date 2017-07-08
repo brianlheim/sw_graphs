f27bdce451a14d0be587574c57d06187eb7c94f4
Brian Heim <brianlheim@gmail.com>
2017-07-08 14:39:43 -0400

    dfs: add the option to parse a file, add program options

--- src/DepthFirstSearchDriver.cpp
+++ src/DepthFirstSearchDriver.cpp
@@ -10,20 +10,63 @@
 #include "ALUGraph.hpp"
 #include "DepthFirstSearch.hpp"
 
-#include <iostream>
+#include <boost/program_options.hpp>
 
+#include <iostream>
+#include <fstream> // ifstream
+#include <string> // string
+
+using std::string;
+using std::ifstream;
 using std::cin;
 using std::cout;
 using std::endl;
 
 using VertexID = AbstractUGraph::VertexID;
+namespace bpo = boost::program_options;
 
-int main()
+void parse_program_options( int argc, char **argv, bpo::options_description& desc, bpo::variables_map& vm )
 {
-  cout << "Driver for depth first search on undirected graph" << endl;
-  cout << "Enter the graph:" << endl;
+  desc.add_options()
+    ("help,h"      , "Show this message")
+    ("input-file,i", "Input file to read")
+    ;
 
-  ALUGraph ug = ALUGraph( cin );
+  // the positional argument is input-file
+  bpo::positional_options_description pod;
+  pod.add( "input-file", 1 );
+
+  bpo::store(
+      bpo::command_line_parser(argc, argv).options(desc).positional(pod).run(),
+      vm
+      );
+  bpo::notify( vm );
+}
+
+ALUGraph parseGraph( bpo::variables_map& vm )
+{
+  if ( vm.count("input-file") ) {
+    string filename = vm["input-file"].as<string>();
+    ifstream file( filename );
+    return ALUGraph( file );
+  } else {
+    return ALUGraph( cin );
+  }
+}
+
+int main( int argc, char **argv )
+{
+  bpo::options_description desc( "Allowed options" );
+  bpo::variables_map vm;
+  parse_program_options( argc, argv, desc, vm );
+
+  // if help is requested, just print help and exit
+  if ( vm.count("help") ) {
+    cout << desc << endl;
+    std::exit(EXIT_SUCCESS);
+  }
+
+  ALUGraph ug = parseGraph( vm );
 
   cout << "The graph is this: " << endl;
   cout << ug.toString();
